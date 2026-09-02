bec57ff4d5b0ec1ad8bf054e3c3f41cb678fd735
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-02 15:26:49 +0330

    Complete responsive design with cover images and full layout

--- food/models.py
+++ food/models.py
@@ -11,3 +11,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=250)
     item_desc = models.CharField(max_length=850)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500,
+        default="https://tse2.mm.bing.net/th/id/OIP.7OYh9Zt5JGrlaCVgiRz17AHaE8?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    )
