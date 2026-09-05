745fa26e6bc0a21384aea12dbd068e5f12e81276
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-05 17:21:24 +0330

    Fix urls.py syntax error

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("food/", include("food.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("food/", include("food.urls")),
+    path("users/", include("users.urls")),
+]
