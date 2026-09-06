bbab23a06426664dbab56ffd0601997b00119669
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-06 12:51:24 +0330

    Add email field to registration using UserCreationForm

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def register(request):
-    HttpResponse("you have to register")
+    form = UserCreationForm()
+    return render(request, "users/register.html", {"form": form})
