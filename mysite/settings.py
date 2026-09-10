7fa0579c96a80e2d8044e696b0a01d6c1d8c0529
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-10 11:11:53 +0330

    Switch from SQLite to PostgreSQL database

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "appdb",
+        "USER": "mh",
+        "PASSWORD": "1388552009mh",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
