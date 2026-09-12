40bc87b25a1a574c2f99678103827a622b9d30e0
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-12 10:48:12 +0330

    update item mode

--- food/models.py
+++ food/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.utils import timezone  # ← این رو اضافه کن
 
 
 class Item(models.Model):
@@ -9,6 +10,10 @@ class Item(models.Model):
     item_desc = models.CharField(max_length=850)
     item_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     item_image = models.CharField(max_length=500, blank=True)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.item_name
