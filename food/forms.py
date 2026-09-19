534f9046b7c8f62f205ef6f4d945bb3173fd246a
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-19 11:28:12 +0330

    add form validation for price < 50

--- food/forms.py
+++ food/forms.py
@@ -9,3 +9,11 @@ class ItemForm(forms.ModelForm):
         widgets = {
             "item_desc": forms.Textarea(attrs={"rows": 4}),
         }
+
+    def clean_item_price(self):
+        price = self.cleaned_data.get("item_price")
+        if price > 50:
+            raise forms.ValidationError(
+                "it is too expensive it should be less than $50"
+            )
+        return price
