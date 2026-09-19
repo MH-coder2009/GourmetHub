4717020864d21471a125806f876366b7476a7639
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-19 12:04:36 +0330

    add form validation for item_name and itme_desc

--- food/forms.py
+++ food/forms.py
@@ -17,3 +17,12 @@ class ItemForm(forms.ModelForm):
                 "it is too expensive it should be less than $50"
             )
         return price
+
+    def clean(self):
+        cleaned_data = super().clean()
+        item_name = cleaned_data.get("item_name")
+        item_desc = cleaned_data.get("item_desc")
+
+        if item_name and item_desc and item_name.lower() in item_desc.lower():
+            raise forms.ValidationError("they shouldnt be same")
+        return cleaned_data
