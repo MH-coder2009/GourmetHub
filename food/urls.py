8903f70c0eba087c6bc4ae5f505fe212c1cae8da
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-21 11:19:26 +0330

    update urls pattern

--- food/urls.py
+++ food/urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = "food"
 
 urlpatterns = [
-    path("item-api/", views.get_list_api, name="get_list_api"),
-    path("item-details/<int:pk>", views.get_details_item, name="get_details_item"),
     path("", cache_page(60 * 15)(views.IndexClassView.as_view()), name="index"),
     path(
         "<int:pk>/",
@@ -16,4 +14,6 @@ urlpatterns = [
     path("add/", views.CreateItemClassView.as_view(), name="create"),
     path("update/<int:pk>/", views.UpdateItemClassView.as_view(), name="update_item"),
     path("delete/<int:pk>/", views.DeleteItemClassView.as_view(), name="delete_item"),
+    path("item-api/", views.get_list_api, name="get_list_api"),
+    path("item-details/<int:pk>/", views.get_details_item, name="get_details_item"),
 ]
