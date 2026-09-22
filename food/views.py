000554cf05c6fdf6a45bfbbbdf8b4c9be2821a57
mhaf <mohammadhoseinafshari930@gmail.com>
2026-09-22 19:30:09 +0330

    add api with class v1

--- food/views.py
+++ food/views.py
@@ -12,24 +12,46 @@ from django.views.generic import (
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Item
 from .serializers import Itemserializers
 from .forms import ItemForm
 
 
-@api_view(["GET", "POST"])
-def get_list_api(request):
-    if request.method == "GET":
+class ItemListAPIView(APIView):
+    def get(self, request):
         items = Item.objects.all()
         serializer = Itemserializers(items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    serializer = Itemserializers(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = Itemserializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user_name=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(["GET", "POST"])
+# def get_list_api(request):
+#     if request.method == "GET":
+#         items = Item.objects.all()
+#         serializer = Itemserializers(items, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     serializer = Itemserializers(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ItemDetailAPI(APIView):
+    def get(self, request, pk):
+        item = get_object_or_404(Item, pk=pk)
+        serializer = Itemserializers(item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(["GET", "PUT", "DELETE"])
